4bc5bb58bbd91071e6771f1232a0db784b2e28ca
Brian Lewis <brian@bayesimpact.org>
2017-11-13 08:37:43 -0800

    Exit early for STRING datatypes

--- teradata/datatypes.py
+++ teradata/datatypes.py
@@ -47,15 +47,15 @@ secondIntervalRegEx = re.compile("^(-?)(\d+\.?\d*)$")
 periodRegEx1 = re.compile("\('(.*)',\s*'(.*)'\)")
 periodRegEx2 = re.compile("ResultStruct:PERIOD\(.*\)\[(.*),\s*(.*)\]")
 
-NUMBER_TYPES = ("BYTEINT", "BIGINT", "DECIMAL", "DOUBLE", "DOUBLE PRECISION",
+NUMBER_TYPES = {"BYTEINT", "BIGINT", "DECIMAL", "DOUBLE", "DOUBLE PRECISION",
                 "INTEGER", "NUMBER", "SMALLINT", "FLOAT", "INT", "NUMERIC",
-                "REAL")
+                "REAL"}
 
-INT_TYPES = ("BYTEINT", "BIGINT", "INTEGER", "SMALLINT", "INT")
+INT_TYPES = {"BYTEINT", "BIGINT", "INTEGER", "SMALLINT", "INT"}
 
-FLOAT_TYPES = ("FLOAT", "DOUBLE", "DOUBLE PRECISION", "REAL")
+FLOAT_TYPES = {"FLOAT", "DOUBLE", "DOUBLE PRECISION", "REAL"}
 
-BINARY_TYPES = ("BLOB", "BYTE", "VARBYTE")
+BINARY_TYPES = {"BLOB", "BYTE", "VARBYTE"}
 
 
 def _getMs(m, num):
@@ -230,7 +230,9 @@ class DefaultDataTypeConverter (DataTypeConverter):
         logger.trace(
             "Converting \"%s\" to (%s, %s).", value, dataType, typeCode)
         if value is not None:
-            if typeCode == NUMBER:
+            if typecode == STRING:
+                return value
+            elif typeCode == NUMBER:
                 try:
                     return NUMBER(value)
                 except:
